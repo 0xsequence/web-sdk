e3d4241aa16f3462041d630df558cd3a67d42f40
Corban Riley <corbanbrook@gmail.com>
2024-05-05 14:50:58 -0400

    Remove styles.css.ts

--- packages/kit/src/components/styles.css.ts
+++
@@ -1,9 +0,0 @@
-import { globalStyle } from '@vanilla-extract/css'
-
-globalStyle('#kit-provider *, #kit-provider::before *, #kit-provider *::after', {
-  boxSizing: 'border-box'
-})
-
-globalStyle('#kit-wallet *, #kit-wallet::before *, #kit-wallet *::after', {
-  boxSizing: 'border-box'
-})
