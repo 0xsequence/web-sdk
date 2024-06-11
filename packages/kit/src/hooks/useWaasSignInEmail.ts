02ff2384f494846aad5a57245747443c923fe0f4
samuelea <batonenbois@gmail.com>
2024-06-11 14:43:50 -0400

    use client directive

--- packages/kit/src/hooks/useWaasSignInEmail.ts
+++ packages/kit/src/hooks/useWaasSignInEmail.ts
@@ -1,4 +1,6 @@
-import React, { useEffect, useState } from 'react'
+'use client'
+
+import { useEffect, useState } from 'react'
 import { useConfig, useAccount } from 'wagmi'
 
 import { LocalStorageKey } from '../constants/localStorage'
