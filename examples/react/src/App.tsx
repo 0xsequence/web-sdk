984222db0cc17d9dafe563ee7b74a4650ed397f5
Corban Riley <corbanbrook@gmail.com>
2024-09-28 00:48:16 -0400

    Using SequenceKit instead of Providers in react example

--- examples/react/src/App.tsx
+++ examples/react/src/App.tsx
@@ -1,33 +1,47 @@
 import '@0xsequence/design-system/styles.css'
 
 import { ThemeProvider } from '@0xsequence/design-system'
-import { KitProvider } from '@0xsequence/kit'
+import { SequenceKit } from '@0xsequence/kit'
 import { KitCheckoutProvider } from '@0xsequence/kit-checkout'
 import { KitWalletProvider } from '@0xsequence/kit-wallet'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { WagmiProvider } from 'wagmi'
+// import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+// import { WagmiProvider } from 'wagmi'
 
 import { Homepage } from './components/Homepage'
 import { kitConfig, wagmiConfig } from './config'
 
-const queryClient = new QueryClient()
+// const queryClient = new QueryClient()
 
 export const App = () => {
+  // return (
+  //   <WagmiProvider config={wagmiConfig}>
+  //     <QueryClientProvider client={queryClient}>
+  //       <KitProvider config={kitConfig}>
+  //         <KitWalletProvider>
+  //           <KitCheckoutProvider>
+  //             <div id="app">
+  //               <ThemeProvider root="#app" scope="app" theme="dark">
+  //                 <Homepage />
+  //               </ThemeProvider>
+  //             </div>
+  //           </KitCheckoutProvider>
+  //         </KitWalletProvider>
+  //       </KitProvider>
+  //     </QueryClientProvider>
+  //   </WagmiProvider>
+  // )
+
   return (
-    <WagmiProvider config={wagmiConfig}>
-      <QueryClientProvider client={queryClient}>
-        <KitProvider config={kitConfig}>
-          <KitWalletProvider>
-            <KitCheckoutProvider>
-              <div id="app">
-                <ThemeProvider root="#app" scope="app" theme="dark">
-                  <Homepage />
-                </ThemeProvider>
-              </div>
-            </KitCheckoutProvider>
-          </KitWalletProvider>
-        </KitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
+    <SequenceKit wagmiConfig={wagmiConfig} kitConfig={kitConfig}>
+      <KitWalletProvider>
+        <KitCheckoutProvider>
+          <div id="app">
+            <ThemeProvider root="#app" scope="app" theme="dark">
+              <Homepage />
+            </ThemeProvider>
+          </div>
+        </KitCheckoutProvider>
+      </KitWalletProvider>
+    </SequenceKit>
   )
 }
