9d7a81cdc8618e82b498738525d02e203f11213c
Andrew Lee <andrewlee011@hotmail.com>
2024-11-04 10:34:35 -0500

    Changed prop ...rest into options

--- packages/kit/src/connectors/email/emailWaas.ts
+++ packages/kit/src/connectors/email/emailWaas.ts
@@ -5,14 +5,13 @@ import { getEmailLogo } from './EmailLogo'
 
 export type EmailWaasOptions = Omit<BaseSequenceWaasConnectorOptions, 'loginType'>
 
-export const emailWaas = ({ ...rest }: EmailWaasOptions): Wallet => ({
+export const emailWaas = (options: EmailWaasOptions): Wallet => ({
   id: 'email-waas',
   logoDark: getEmailLogo({ isDarkMode: true }),
   logoLight: getEmailLogo({ isDarkMode: false }),
   name: 'Email',
   type: 'social',
   createConnector: () => {
-    const options = { ...rest }
     const connector = sequenceWaasWallet({
       ...options,
       loginType: 'email'
