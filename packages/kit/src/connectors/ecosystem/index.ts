543d9a85041d7ebcc8d22b5325bc2cade1738604
Tolgahan Arikan <tolgahan.arikan@gmail.com>
2025-02-17 17:44:27 +0300

    Update

--- packages/kit/src/connectors/ecosystem/index.ts
+++ packages/kit/src/connectors/ecosystem/index.ts
@@ -1,2 +1,17 @@
-export { ecosystemWallet } from './ecosystemWallet'
-export type { EcosystemWalletOptions } from './ecosystemWallet'
+import { Wallet, WalletProperties } from '../../types'
+
+import { ecosystemWallet as baseEcosystemWallet, BaseEcosystemConnectorOptions } from './ecosystemWallet'
+
+export type EcosystemWalletOptions = BaseEcosystemConnectorOptions &
+  Pick<WalletProperties, 'logoDark' | 'logoLight'> & {
+    name?: string
+  }
+
+export const ecosystemWallet = (options: EcosystemWalletOptions): Wallet => ({
+  id: 'ecosystem-wallet',
+  logoDark: options.logoDark,
+  logoLight: options.logoLight,
+  name: options.name || 'Ecosystem Wallet',
+  type: 'social',
+  createConnector: () => baseEcosystemWallet(options)
+})
