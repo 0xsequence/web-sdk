5c3d4e966b8e2a2c6f0b2ca7601b9f30e2100a9f
Corban Riley <corbanbrook@gmail.com>
2025-03-06 14:05:50 -0500

    Fix PoweredBySequence component alignment

--- packages/kit/src/components/SequenceLogo/PoweredBySequence.tsx
+++ packages/kit/src/components/SequenceLogo/PoweredBySequence.tsx
@@ -5,7 +5,7 @@ import { SequenceLogo } from './SequenceLogo'
 export const PoweredBySequence = () => {
   return (
     <div
-      className="powered-by-sequence-footer flex relative gap-2 flex-row items-center justify-center select-none cursor-pointer left-[-28px]"
+      className="powered-by-sequence-footer flex relative gap-1 flex-row items-center justify-center select-none cursor-pointer"
       onClick={() => {
         if (typeof window !== 'undefined') {
           window.open('https://sequence.xyz')
@@ -15,9 +15,7 @@ export const PoweredBySequence = () => {
       <Text variant="xsmall" color="muted" fontWeight="bold">
         Powered by
       </Text>
-      <div className="h-5 w-5 relative top-[1px]">
-        <SequenceLogo />
-      </div>
+      <SequenceLogo />
     </div>
   )
 }
