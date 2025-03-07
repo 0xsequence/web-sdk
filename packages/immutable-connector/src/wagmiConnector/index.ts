266f55a3902f7fbb58c46a20d5afe08ce6e5f9bb
samuelea <batonenbois@gmail.com>
2025-03-06 20:43:44 -0500

    immutable passport connector

--- packages/immutable-connector/src/wagmiConnector/index.ts
+++ packages/immutable-connector/src/wagmiConnector/index.ts
@@ -1,103 +1,120 @@
+import { passport } from '@imtbl/sdk'
+import { Environment } from '@imtbl/config'
+import { type IMXProvider } from '@imtbl/x-provider'
+import { type Address } from 'viem'
 import { createConnector } from 'wagmi'
-import { passport, config } from '@imtbl/sdk'
 
-export interface BaseImmutableConnectorOptions {}
+export interface BaseImmutableConnectorOptions {
+  environment?: Environment,
+  redirectUri?: string,
+  logoutRedirectUri?: string,
+  publishableKey?: string,
+  clientId?: string,
+}
 
 immutableConnector.type = 'immutable' as const
 
 export function immutableConnector(params: BaseImmutableConnectorOptions) {
-  type Provider = any
+  type Provider = IMXProvider
   type Properties = {
     params: BaseImmutableConnectorOptions
   }
   type StorageItem = {}
 
-  let provider: any = undefined
+  let provider: IMXProvider | undefined = undefined
+
+  const IMMUTABLE_CHAINS = {
+    [Environment.SANDBOX]: 13473,
+    [Environment.PRODUCTION]: 13371
+  } as const;
+
+  const { environment = Environment.SANDBOX } = params
+
+  const passportInstance = new passport.Passport({
+    baseConfig: {
+      environment,
+      publishableKey: params.publishableKey || 'pk_imapik-test-lnUqNu5uDYfKDz_stwpN'
+    },
+    clientId: params.clientId || '420lberPuiZaO6SBX6Anoa7C9kpcsuer',
+    redirectUri: params.redirectUri || window.location.origin,
+    logoutRedirectUri: params.logoutRedirectUri,
+    audience: 'platform_api',
+    scope: 'openid offline_access email transact',
+  });
 
   return createConnector<Provider, Properties, StorageItem>(config => ({
     id: 'immutable',
     name: 'Immutable Passport',
     type: immutableConnector.type,
     params,
+    passportInstance,
 
     async setup() {
-      const passportInstance = new passport.Passport({
-        baseConfig: {
-          // latest immutable SDK is missing the environment config from the docs
-          // @ts-ignore-next-line
-          environment: 'sandbox',
-          publishableKey: 'pk_imapik-test-lnUqNu5uDYfKDz_stwpN'
-        },
-        clientId: '420lberPuiZaO6SBX6Anoa7C9kpcsuer',
-        redirectUri: 'http://localhost:4444', // replace with one of your redirect URIs from Hub
-        logoutRedirectUri: 'http://localhost:4444', // replace with one of your logout URIs from Hub
-        audience: 'platform_api',
-        scope: 'openid offline_access email transact',
-      });
     },
 
     async connect() {
-      const provider = await this.getProvider()
+      await passportInstance.login({ useCachedSession: true });
+    
+      const provider = (await this.getProvider()) as IMXProvider;
 
-      return { accounts: [], chainId: 1 }
+      const isRegistered = await provider.isRegisteredOffchain();
+      if (!isRegistered) {
+        await provider.registerOffchain();
+      }
+
+      const accounts = await this.getAccounts();
+      const chainId = await this.getChainId();
+
+      return { accounts, chainId }
     },
 
     async disconnect() {
-      const provider = await this.getProvider()
-
-      provider.disconnect()
+      await passportInstance.logout()
+      provider = undefined
+      config.emitter.emit('disconnect')
     },
 
     async getAccounts() {
-      const provider = await this.getProvider()
+      const provider = (await this.getProvider()) as IMXProvider
 
-      return []
+      const account = await provider.getAddress()
+
+      return [account as Address]
     },
 
-    async getProvider() {
+    async getProvider(): Promise<IMXProvider> {
       if (provider) {
         return provider
       }
 
-      // return the immutable passport provider
-      provider = {}
+      provider = await passportInstance.connectImx();
+
+      return provider 
     },
 
     async isAuthorized() {
-      return false
-      // try {
-      //   const account = await this.getAccounts()
-      //   return !!account
-      // } catch (e) {
-      //   return false
-      // }
+      try {
+        const provider = await this.getProvider();
+        const address = await provider.getAddress();
+        return Boolean(address);
+      } catch {
+        return false;
+      }
     },
 
-    async switchChain({ chainId }) {
-      const provider = await this.getProvider()
-
-      const chain = config.chains.find(c => c.id === chainId) || config.chains[0]
-
-      config.emitter.emit('change', { chainId })
-
-      return chain
+    async switchChain() {
+      throw new Error('Chain switching is not supported by Immutable Passport');
     },
 
     async getChainId() {
-      const provider = await this.getProvider()
-      const chainId = provider.getChainId()
-
-      return chainId
+      return IMMUTABLE_CHAINS[environment]
     },
 
     async onAccountsChanged(accounts) {
       return { account: accounts[0] }
     },
 
-    async onChainChanged(chain) {
-      const provider = await this.getProvider()
-
-      config.emitter.emit('change', { chainId: normalizeChainId(chain) })
+    async onChainChanged() {
     },
 
     async onConnect(_connectinfo) {},
@@ -106,11 +123,4 @@ export function immutableConnector(params: BaseImmutableConnectorOptions) {
       config.emitter.emit('disconnect')
     }
   }))
-}
-
-function normalizeChainId(chainId: string | number | bigint | { chainId: string }) {
-  if (typeof chainId === 'object') return normalizeChainId(chainId.chainId)
-  if (typeof chainId === 'string') return Number.parseInt(chainId, chainId.trim().substring(0, 2) === '0x' ? 16 : 10)
-  if (typeof chainId === 'bigint') return Number(chainId)
-  return chainId
 }
