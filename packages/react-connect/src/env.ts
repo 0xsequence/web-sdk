d1c1925e635a6d429d1dccdd17cb3d2a89b832b2
samuelea <batonenbois@gmail.com>
2025-03-13 12:00:33 -0400

    dev environment improvements. Added example for next.js

--- packages/react-connect/src/env.ts
+++ packages/react-connect/src/env.ts
@@ -6,28 +6,22 @@ declare global {
   var __WEB_SDK_DEV_SARDINE_PROJECT_ACCESS_KEY__: string | undefined
 }
 
-export const DEV_GLOBAL = !!globalThis.__WEB_SDK_DEV_GLOBAL__
-export const DEV_SARDINE = !!globalThis.__WEB_SDK_DEV_SARDINE__
-export const DEV_TRANSAK = !!globalThis.__WEB_SDK_DEV_TRANSAK__
-export const DEV_SEQUENCE_APIS = !!globalThis.__WEB_SDK_DEV_SEQUENCE_APIS__
-export const DEV_SARDINE_PROJECT_ACCESS_KEY = globalThis.__WEB_SDK_DEV_SARDINE_PROJECT_ACCESS_KEY__
-
 export const isDev = () => {
-  return DEV_GLOBAL
+  return !!globalThis.__WEB_SDK_DEV_GLOBAL__
 }
 
 export const isDevSardine = () => {
-  return DEV_SARDINE || isDev()
+  return !!globalThis.__WEB_SDK_DEV_SARDINE__ || isDev()
 }
 
 export const isDevTransak = () => {
-  return DEV_TRANSAK || isDev()
+  return !!globalThis.__WEB_SDK_DEV_TRANSAK__ || isDev()
 }
 
 export const isDevSequenceApis = () => {
-  return DEV_SEQUENCE_APIS || isDev()
+  return !!globalThis.__WEB_SDK_DEV_SEQUENCE_APIS__ || isDev()
 }
 
 export const getDevSardineProjectAccessKey = (prodProjectAccessKey: string) => {
-  return DEV_SARDINE_PROJECT_ACCESS_KEY || prodProjectAccessKey
+  return globalThis.__WEB_SDK_DEV_SARDINE_PROJECT_ACCESS_KEY__ || prodProjectAccessKey
 }
