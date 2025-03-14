b87bf9b463adc3ec84ec6b881a39d2257737b21e
samuelea <batonenbois@gmail.com>
2025-03-14 13:55:36 -0400

    sardine on ramp

---
+++ packages/checkout/src/views/PaymentSelection/FundWithFiat.tsx
@@ -0,0 +1,47 @@
+import { findSupportedNetwork } from '@0xsequence/network'
+import { ArrowRightIcon, Card, CurrencyIcon, Text } from '@0xsequence/design-system'
+import { TransactionOnRampProvider } from '@0xsequence/marketplace'
+
+import { useSelectPaymentModal, useAddFundsModal } from '../../hooks'
+
+interface FundWithFiatProps {
+  walletAddress: string
+  provider: TransactionOnRampProvider
+  chainId?: number
+}
+
+export const FundWithFiat = ({ walletAddress, provider, chainId }: FundWithFiatProps) => {
+  const { triggerAddFunds } = useAddFundsModal()
+  const { closeSelectPaymentModal, selectPaymentSettings } = useSelectPaymentModal()
+
+  const getNetworks = (): string | undefined => {
+    const chain = selectPaymentSettings?.chain
+    if (!chain) return
+
+    const network = findSupportedNetwork(chain)
+    return network?.name?.toLowerCase()
+  }
+
+  const onClick = () => {
+    closeSelectPaymentModal()
+    triggerAddFunds({
+      walletAddress,
+      provider,
+      networks: getNetworks()
+    })
+  }
+
+  return (
+    <Card key="sardine" className="flex items-center justify-between p-4 cursor-pointer" onClick={onClick}>
+      <div className="flex flex-row gap-3 items-center">
+        <CurrencyIcon color="white" />
+        <Text color="text100" variant="normal" fontWeight="bold">
+          Fund wallet with credit card
+        </Text>
+      </div>
+      <div style={{ transform: 'rotate(-45deg)' }}>
+        <ArrowRightIcon color="white" />
+      </div>
+    </Card>
+  )
+}
