400201ab2a82e28b77a2133b8a442f40a1d006e1
samuelea <batonenbois@gmail.com>
2025-03-20 12:01:32 -0400

    move common hooks in useCheckoutUI

--- packages/checkout/src/hooks/useCheckoutUI/index.tsx
+++ packages/checkout/src/hooks/useCheckoutUI/index.tsx
@@ -1,6 +1,13 @@
-import { Collectible } from '../../contexts/SelectPaymentModal'
-import { useOrderSummary, type UseOrderSummaryArgs, type UseOrderSummaryReturn } from './useOrderSummary'
+import { useGetTokenMetadata, useGetContractInfo } from '@0xsequence/hooks'
+import { findSupportedNetwork } from '@0xsequence/network'
 
+import React from 'react'
+import { Hex } from 'viem'
+
+import { useOrderSummary, type UseOrderSummaryArgs, type UseOrderSummaryReturn } from './useOrderSummary'
+import { useCreditCardPayment, type UseCreditCardPaymentArgs, type UseCreditCardPaymentReturn } from './useCreditCardPayment'
+import { Collectible } from '../../contexts/SelectPaymentModal'
+import { TransakConfig } from '../../contexts/CheckoutModal'
 // crypto payment
 
 // credit card payment
@@ -12,14 +19,18 @@ interface UseCheckoutUIArgs {
   collectibles: Collectible[]
   collectionAddress: string
   recipientAddress: string
+  targetContractAddress: string
+  txData: Hex
   transactionConfirmations?: number
+  creditCardProvider?: 'sardine' | 'transak'
+  transakConfig?: TransakConfig
   onSuccess?: (txHash: string) => void
   onError?: (error: Error) => void
-  onClose?: () => void
 }
 
 interface UseCheckoutUIReturn {
   useOrderSummary: (args: UseOrderSummaryArgs) => UseOrderSummaryReturn
+  // useCreditCardPayment: (args: UseCreditCardPaymentArgs) => UseCreditCardPaymentReturn
 }
 
 export const useCheckoutUI = ({
@@ -31,13 +42,53 @@ export const useCheckoutUI = ({
   recipientAddress,
   transactionConfirmations
 }: UseCheckoutUIArgs): UseCheckoutUIReturn => {
+  const network = findSupportedNetwork(chain)
+  const chainId = network?.chainId || 137
+
+  const {
+    data: tokenMetadatas,
+    isLoading: isLoadingTokenMetadatas,
+    error: errorTokenMetadata
+  } = useGetTokenMetadata({
+    chainID: String(chainId),
+    contractAddress: collectionAddress,
+    tokenIDs: collectibles.map(c => c.tokenId)
+  })
+
+  const {
+    data: dataCollectionInfo,
+    isLoading: isLoadingCollectionInfo,
+    error: errorCollectionInfo
+  } = useGetContractInfo({
+    chainID: String(chainId),
+    contractAddress: collectionAddress
+  })
+
+  const {
+    data: currencyInfo,
+    isLoading: isLoadingCurrencyInfo,
+    error: errorCurrencyInfo
+  } = useGetContractInfo({
+    chainID: String(chainId),
+    contractAddress: currencyAddress
+  })
+
   return {
     useOrderSummary: useOrderSummary({
       chain,
       currencyAddress,
       totalPriceRaw,
       collectibles,
-      collectionAddress
+      collectionAddress,
+      currencyInfo,
+      tokenMetadatas,
+      dataCollectionInfo,
+      isLoadingCollectionInfo,
+      errorCollectionInfo,
+      isLoadingCurrencyInfo,
+      isLoadingTokenMetadatas,
+      errorTokenMetadata,
+      errorCurrencyInfo
     })
   }
 }
