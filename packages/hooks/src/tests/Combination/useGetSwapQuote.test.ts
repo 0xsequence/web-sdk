2a6c2809254b1b9540ec672d4a1b3be7a01959f8
VGabriel45 <vgabrielmarian21@gmail.com>
2025-04-02 14:25:32 +0300

    chore: added test for api

--- packages/hooks/src/tests/Combination/useGetSwapQuote.test.ts
+++ packages/hooks/src/tests/Combination/useGetSwapQuote.test.ts
@@ -2,22 +2,23 @@ import { renderHook, waitFor } from '@testing-library/react'
 import { HttpResponse, http } from 'msw'
 import { describe, expect, it } from 'vitest'
 
-import { ACCOUNT_ADDRESS, NATIVE_TOKEN_ADDRESS_0X_SWAP } from '../../constants'
+import { ACCOUNT_ADDRESS, ZERO_ADDRESS } from '../../constants'
 import { useGetSwapQuote } from '../../hooks/Combination/useGetSwapQuote'
 import { createWrapper } from '../createWrapper'
 import { server } from '../setup'
 
 const getSwapQuoteArgs = {
   userAddress: ACCOUNT_ADDRESS,
-  buyCurrencyAddress: NATIVE_TOKEN_ADDRESS_0X_SWAP,
-  sellCurrencyAddress: NATIVE_TOKEN_ADDRESS_0X_SWAP,
+  buyCurrencyAddress: ZERO_ADDRESS,
+  sellCurrencyAddress: ZERO_ADDRESS,
   buyAmount: '20000',
   chainId: 1,
   includeApprove: true
 }
 
-describe('useGetSwapQuote', () => {
+describe('useGetSwapQuoteV2', () => {
   it('should return data with a balance', async () => {
+
     const { result } = renderHook(() => useGetSwapQuote(getSwapQuoteArgs), {
       wrapper: createWrapper()
     })
@@ -43,5 +44,26 @@ describe('useGetSwapQuote', () => {
     })
 
     await waitFor(() => expect(result.current.isError).toBe(true))
+  })
+
+  it('should return correct data from dev api', async () => {
+    const response = await fetch('https://dev-api.sequence.app/rpc/API/GetSwapQuoteV2', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Access-Key': 'AQAAAAAAAAK2JvvZhWqZ51riasWBftkrVXE'
+      },
+      body: JSON.stringify({
+        userAddress: ACCOUNT_ADDRESS,
+        buyCurrencyAddress: ZERO_ADDRESS,
+        sellCurrencyAddress: ZERO_ADDRESS,
+        buyAmount: '20000',
+        chainId: 1,
+        includeApprove: true
+      })
+    })
+
+    const data = await response.json()
+    console.log('Real API Response Structure:', JSON.stringify(data, null, 2))
   })
 })
