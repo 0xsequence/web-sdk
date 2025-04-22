5ba8d7171245e3ffef7e241b8641565a3a3cb4bb
samuelea <batonenbois@gmail.com>
2025-04-22 18:06:09 -0400

    add usePaymentIntent flow

--- packages/checkout/src/hooks/useFortePaymentIntent.ts
+++ packages/checkout/src/hooks/useFortePaymentIntent.ts
@@ -1,17 +1,24 @@
 import { useQuery } from '@tanstack/react-query'
 
 import { createFortePaymentIntent, CreateFortePaymentIntentArgs } from '../api/data'
+import { useEnvironmentContext } from '../contexts/Environment'
+interface UseFortePaymentIntentOptions {
+  disabled?: boolean
+}
 
-export const useFortePaymentIntent = (args: CreateFortePaymentIntentArgs) => {
+export const useFortePaymentIntent = (args: CreateFortePaymentIntentArgs, options?: UseFortePaymentIntentOptions) => {
+  const { fortePaymentUrl } = useEnvironmentContext()
+
   return useQuery({
     queryKey: ['useFortePaymentIntent', args],
     queryFn: async () => {
-      const res = await createFortePaymentIntent(args)
+      const res = await createFortePaymentIntent(fortePaymentUrl, args)
 
       return res
     },
     retry: false,
     staleTime: 60 * 1000,
-    refetchOnWindowFocus: false
+    refetchOnWindowFocus: false,
+    enabled: !options?.disabled
   })
 }
