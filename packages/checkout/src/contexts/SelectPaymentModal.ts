b478bf5a8733df1a555cfe34b2879d11239b1ce3
samuelea <batonenbois@gmail.com>
2025-04-27 21:27:46 -0400

    forte config

--- packages/checkout/src/contexts/SelectPaymentModal.ts
+++ packages/checkout/src/contexts/SelectPaymentModal.ts
@@ -1,7 +1,7 @@
 import { TransactionOnRampProvider } from '@0xsequence/marketplace'
 import type { Hex } from 'viem'
 
-import type { TransakConfig } from '../contexts/CheckoutModal.js'
+import type { ForteConfig, TransakConfig } from '../contexts/CheckoutModal.js'
 
 import { createGenericContext } from './genericContext.js'
 
@@ -45,6 +45,7 @@ export interface SelectPaymentSettings {
   creditCardProviders?: string[]
   transakConfig?: TransakConfig
   sardineConfig?: SardineConfig
+  forteConfig?: ForteConfig
   customProviderCallback?: (onSuccess: (txHash: string) => void, onError: (error: Error) => void, onClose: () => void) => void
   supplementaryAnalyticsInfo?: SupplementaryAnalyticsInfo
   skipNativeBalanceCheck?: boolean
