6d0c6a8eb595457f32ea6e74aaa1717ed374f016
samuelea <batonenbois@gmail.com>
2025-05-01 10:17:47 -0400

    return null from the widget loader

--- packages/checkout/src/views/PendingCreditCardTransaction.tsx
+++ packages/checkout/src/views/PendingCreditCardTransaction.tsx
@@ -633,5 +633,5 @@ export function ForteWidgetLoader({ widgetData }: ForteWidgetLoaderProps) {
     }
   }, [widgetData])
 
-  return <div id="forte-payments-widget-container"></div>
+  return null
 }
