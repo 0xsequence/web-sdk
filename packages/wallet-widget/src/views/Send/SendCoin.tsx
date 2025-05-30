7234603401e8c7c5c52646a09d0f3803263fbb40
Andrew Lee <andrewlee011@hotmail.com>
2025-05-29 17:48:14 -0700

    feat: added wallet operation popover in home screen, feat: added functionality to operation popover in home screen

--- packages/wallet-widget/src/views/Send/SendCoin.tsx
+++ packages/wallet-widget/src/views/Send/SendCoin.tsx
@@ -35,7 +35,7 @@ import { useEffect, useRef, useState, type ChangeEvent } from 'react'
 import { encodeFunctionData, formatUnits, parseUnits, toHex, zeroAddress, type Hex } from 'viem'
 import { useAccount, useChainId, useConfig, usePublicClient, useSwitchChain, useWalletClient } from 'wagmi'
 
-import { WalletSelect } from '../../components/Select/WalletSelect.js'
+import { ConnectedWalletSelect } from '../../components/Select/ConnectedWalletSelect.js'
 import { SendItemInfo } from '../../components/SendItemInfo.js'
 import { TransactionConfirmation } from '../../components/TransactionConfirmation.js'
 import { EVENT_SOURCE, EVENT_TYPES } from '../../constants/analytics.js'
@@ -391,7 +391,7 @@ export const SendCoin = ({ chainId, contractAddress }: SendCoinProps) => {
                     />
                   }
                 />
-                {wallets.length > 1 && <WalletSelect selectedWallet={toAddress} onClick={setToAddress} />}
+                {wallets.length > 1 && <ConnectedWalletSelect selectedWallet={toAddress} onClick={setToAddress} />}
               </>
             )}
           </div>
