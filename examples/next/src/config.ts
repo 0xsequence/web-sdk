2ea3351675b338bd19bc96f4c6f0809fa5f77397
VGabriel45 <vgabrielmarian21@gmail.com>
2025-06-06 14:01:48 +0300

    update worker demo url

--- examples/next/src/config.ts
+++ examples/next/src/config.ts
@@ -70,7 +70,7 @@ export const config = createConfig('waas', {
     clientId: '970987756660-35a6tc48hvi8cev9cnknp0iugv9poa23.apps.googleusercontent.com'
   },
   epic: {
-    authUrl: 'https://epic-auth-worker-websdk.gmar.workers.dev/login'
+    authUrl: 'https://epic-auth-worker-websdk.tpin.workers.dev/login'
   },
   apple: {
     clientId: 'com.horizon.sequence.waas',
--- examples/react/src/config.ts
+++ examples/react/src/config.ts
@@ -112,7 +112,7 @@ export const config =
             : '970987756660-35a6tc48hvi8cev9cnknp0iugv9poa23.apps.googleusercontent.com'
         },
         epic: {
-          authUrl: 'https://epic-auth-worker-websdk.gmar.workers.dev/login'
+          authUrl: 'https://epic-auth-worker-websdk.tpin.workers.dev/login'
         },
         apple: {
           clientId: 'com.horizon.sequence.waas',
