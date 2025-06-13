92fae8ccd03b6cf8d875e0171b71f5e57918e307
samuelea <batonenbois@gmail.com>
2025-06-13 15:59:47 -0400

    added logic for pay with crypto

--- packages/checkout/src/views/PaymentSelection/PayWithCrypto/index.tsx
+++ packages/checkout/src/views/PaymentSelection/PayWithCrypto/index.tsx
@@ -1,22 +1,33 @@
-import type { LifiSwapRoute } from '@0xsequence/api'
+import type { LifiSwapRoute, LifiToken } from '@0xsequence/api'
 import {
   compareAddress,
   ContractVerificationStatus,
   CryptoOption,
   formatDisplay,
+  sendTransactions,
   TRANSACTION_CONFIRMATIONS_DEFAULT,
   useAnalyticsContext
 } from '@0xsequence/connect'
 import { AddIcon, Button, ChevronDownIcon, Spinner, SubtractIcon, Text, TokenImage } from '@0xsequence/design-system'
-import { useClearCachedBalances, useGetCoinPrices, useGetContractInfo, useGetTokenBalancesSummary } from '@0xsequence/hooks'
+import {
+  useClearCachedBalances,
+  useGetCoinPrices,
+  useGetContractInfo,
+  useGetTokenBalancesSummary,
+  useIndexerClient,
+  useGetSwapRoutes,
+  useGetSwapQuote
+} from '@0xsequence/hooks'
 import { findSupportedNetwork } from '@0xsequence/network'
 import { motion } from 'motion/react'
 import { Fragment, useEffect, useMemo, useState, type SetStateAction } from 'react'
-import { formatUnits, zeroAddress } from 'viem'
-import { useAccount } from 'wagmi'
+import { encodeFunctionData, formatUnits, zeroAddress, type Hex } from 'viem'
+import { useAccount, useWalletClient, usePublicClient, useReadContract } from 'wagmi'
 
+import { EVENT_SOURCE } from '../../../constants/index.js'
+import { ERC_20_CONTRACT_ABI } from '../../../constants/abi.js'
 import type { SelectPaymentSettings } from '../../../contexts/SelectPaymentModal.js'
-import { useSelectPaymentModal, useTransactionStatusModal } from '../../../hooks/index.js'
+import { useSelectPaymentModal, useTransactionStatusModal, useSkipOnCloseCallback } from '../../../hooks/index.js'
 
 interface PayWithCryptoProps {
   settings: SelectPaymentSettings
@@ -31,10 +42,12 @@ interface PayWithCryptoProps {
 const MAX_OPTIONS = 3
 
 export const PayWithCryptoTab = () => {
+  const { clearCachedBalances } = useClearCachedBalances()
   const [isPurchasing, setIsPurchasing] = useState<boolean>(false)
   const { openTransactionStatusModal } = useTransactionStatusModal()
-  const { selectPaymentSettings = {} as SelectPaymentSettings } = useSelectPaymentModal()
+  const { selectPaymentSettings = {} as SelectPaymentSettings, closeSelectPaymentModal } = useSelectPaymentModal()
   const { analytics } = useAnalyticsContext()
+  const [isError, setIsError] = useState<boolean>(false)
 
   const {
     chain,
@@ -48,7 +61,6 @@ export const PayWithCryptoTab = () => {
     enableTransferFunds = true,
     enableMainCurrencyPayment = true,
     enableSwapPayments = true,
-    creditCardProviders = [],
     transactionConfirmations = TRANSACTION_CONFIRMATIONS_DEFAULT,
     onRampProvider,
     onSuccess = () => {},
@@ -58,8 +70,18 @@ export const PayWithCryptoTab = () => {
     slippageBps
   } = selectPaymentSettings
 
+  const { skipOnCloseCallback } = useSkipOnCloseCallback(onClose)
   const network = findSupportedNetwork(chain)
   const chainId = network?.chainId || 137
+
+  const { address: userAddress, connector } = useAccount()
+  const { data: walletClient } = useWalletClient({
+    chainId: chainId
+  })
+  const publicClient = usePublicClient({
+    chainId: chainId
+  })
+  const indexerClient = useIndexerClient(chainId)
 
   const [selectedCurrency, setSelectedCurrency] = useState<{
     currencyAddress: string
@@ -81,7 +103,57 @@ export const PayWithCryptoTab = () => {
     contractAddress: selectPaymentSettings!.currencyAddress
   })
 
-  const isLoading = isLoadingCoinPrice || isLoadingCurrencyInfo
+  const buyCurrencyAddress = currencyAddress
+
+  const { data: swapRoutes = [], isLoading: swapRoutesIsLoading } = useGetSwapRoutes(
+    {
+      walletAddress: userAddress ?? '',
+      chainId,
+      toTokenAmount: price,
+      toTokenAddress: currencyAddress
+    },
+    { disabled: !enableSwapPayments, retry: 3 }
+  )
+
+  const disableSwapQuote = !selectedCurrency || compareAddress(selectedCurrency.currencyAddress, buyCurrencyAddress)
+
+  const { data: swapQuote, isLoading: isLoadingSwapQuote } = useGetSwapQuote(
+    {
+      params: {
+        walletAddress: userAddress ?? '',
+        toTokenAddress: buyCurrencyAddress,
+        fromTokenAddress: selectedCurrency.currencyAddress || '',
+        toTokenAmount: price,
+        chainId: chainId,
+        includeApprove: true,
+        slippageBps: slippageBps || 100
+      }
+    },
+    {
+      disabled: disableSwapQuote
+    }
+  )
+
+  const isNativeToken = compareAddress(selectedCurrency.currencyAddress, zeroAddress)
+  const { data: allowanceData, isLoading: allowanceIsLoading } = useReadContract({
+    abi: ERC_20_CONTRACT_ABI,
+    functionName: 'allowance',
+    chainId: chainId,
+    address: currencyAddress as Hex,
+    args: [userAddress, approvedSpenderAddress || targetContractAddress],
+    query: {
+      enabled: !!userAddress && !isNativeToken
+    }
+  })
+
+  const isLoading =
+    isLoadingCoinPrice ||
+    isLoadingCurrencyInfo ||
+    (allowanceIsLoading && !isNativeToken) ||
+    swapRoutesIsLoading ||
+    isLoadingSwapQuote
+
+  const isApproved: boolean = (allowanceData as bigint) >= BigInt(price) || isNativeToken
 
   const formattedPrice = formatUnits(BigInt(selectPaymentSettings!.price), dataCurrencyInfo?.decimals || 0)
   const displayPrice = formatDisplay(formattedPrice, {
@@ -92,6 +164,260 @@ export const PayWithCryptoTab = () => {
 
   const fiatExchangeRate = coinPricesData?.[0].price?.value || 0
   const priceFiat = (fiatExchangeRate * Number(formattedPrice)).toFixed(2)
+
+  const onPurchaseMainCurrency = async () => {
+    if (!walletClient || !userAddress || !publicClient || !indexerClient || !connector) {
+      return
+    }
+
+    setIsPurchasing(true)
+    setIsError(false)
+
+    try {
+      const walletClientChainId = await walletClient.getChainId()
+      if (walletClientChainId !== chainId) {
+        await walletClient.switchChain({ id: chainId })
+      }
+
+      const approveTxData = encodeFunctionData({
+        abi: ERC_20_CONTRACT_ABI,
+        functionName: 'approve',
+        args: [approvedSpenderAddress || targetContractAddress, price]
+      })
+
+      const transactions = [
+        ...(isApproved
+          ? []
+          : [
+              {
+                to: currencyAddress as Hex,
+                data: approveTxData,
+                chainId
+              }
+            ]),
+        {
+          to: targetContractAddress as Hex,
+          data: txData,
+          chainId,
+          ...(isNativeToken
+            ? {
+                value: BigInt(price)
+              }
+            : {})
+        }
+      ]
+
+      const txHash = await sendTransactions({
+        chainId,
+        senderAddress: userAddress,
+        publicClient,
+        walletClient,
+        indexerClient,
+        connector,
+        transactions,
+        transactionConfirmations,
+        waitConfirmationForLastTransaction: false
+      })
+
+      analytics?.track({
+        event: 'SEND_TRANSACTION_REQUEST',
+        props: {
+          ...supplementaryAnalyticsInfo,
+          type: 'crypto',
+          source: EVENT_SOURCE,
+          chainId: String(chainId),
+          listedCurrency: currencyAddress,
+          purchasedCurrency: currencyAddress,
+          origin: window.location.origin,
+          from: userAddress,
+          to: targetContractAddress,
+          item_ids: JSON.stringify(collectibles.map(c => c.tokenId)),
+          item_quantities: JSON.stringify(collectibles.map(c => c.quantity)),
+          currencySymbol: dataCurrencyInfo?.symbol || '',
+          collectionAddress,
+          txHash
+        },
+        nums: {
+          currencyValue: Number(price),
+          currencyValueDecimal: Number(formatUnits(BigInt(price), dataCurrencyInfo?.decimals || 18))
+        }
+      })
+
+      closeSelectPaymentModal()
+
+      skipOnCloseCallback()
+
+      openTransactionStatusModal({
+        chainId,
+        currencyAddress,
+        collectionAddress,
+        txHash,
+        items: collectibles.map(collectible => ({
+          tokenId: collectible.tokenId,
+          quantity: collectible.quantity,
+          decimals: collectible.decimals,
+          price: collectible.price || price
+        })),
+        onSuccess: () => {
+          clearCachedBalances()
+          onSuccess(txHash)
+        },
+        onClose
+      })
+    } catch (e) {
+      console.error('Failed to purchase...', e)
+      onError(e as Error)
+      setIsError(true)
+    }
+
+    setIsPurchasing(false)
+  }
+
+  const onClickPurchaseSwap = async (swapTokenOption: LifiToken) => {
+    if (!walletClient || !userAddress || !publicClient || !userAddress || !connector || !swapQuote) {
+      return
+    }
+
+    setIsPurchasing(true)
+    setIsError(false)
+
+    try {
+      const walletClientChainId = await walletClient.getChainId()
+      if (walletClientChainId !== chainId) {
+        await walletClient.switchChain({ id: chainId })
+      }
+
+      const approveTxData = encodeFunctionData({
+        abi: ERC_20_CONTRACT_ABI,
+        functionName: 'approve',
+        args: [approvedSpenderAddress || targetContractAddress, price]
+      })
+
+      const isSwapNativeToken = compareAddress(zeroAddress, swapTokenOption.address)
+
+      const transactions = [
+        // Swap quote optional approve step
+        ...(swapQuote?.approveData && !isSwapNativeToken
+          ? [
+              {
+                to: swapTokenOption.address as Hex,
+                data: swapQuote.approveData as Hex,
+                chain: chainId
+              }
+            ]
+          : []),
+        // Swap quote tx
+        {
+          to: swapQuote.to as Hex,
+          data: swapQuote.transactionData as Hex,
+          chain: chainId,
+          ...(isSwapNativeToken
+            ? {
+                value: BigInt(swapQuote.transactionValue)
+              }
+            : {})
+        },
+        // Actual transaction optional approve step
+        ...(isApproved || isNativeToken
+          ? []
+          : [
+              {
+                to: currencyAddress as Hex,
+                data: approveTxData as Hex,
+                chainId: chainId
+              }
+            ]),
+        // transaction on the contract
+        {
+          to: targetContractAddress as Hex,
+          data: txData as Hex,
+          chainId,
+          ...(isNativeToken
+            ? {
+                value: BigInt(price)
+              }
+            : {})
+        }
+      ]
+
+      const txHash = await sendTransactions({
+        chainId,
+        senderAddress: userAddress,
+        publicClient,
+        walletClient,
+        indexerClient,
+        connector,
+        transactions,
+        transactionConfirmations,
+        waitConfirmationForLastTransaction: false
+      })
+
+      analytics?.track({
+        event: 'SEND_TRANSACTION_REQUEST',
+        props: {
+          ...supplementaryAnalyticsInfo,
+          type: 'crypto',
+          source: EVENT_SOURCE,
+          chainId: String(chainId),
+          listedCurrency: swapTokenOption.address,
+          purchasedCurrency: currencyAddress,
+          origin: window.location.origin,
+          from: userAddress,
+          to: targetContractAddress,
+          item_ids: JSON.stringify(collectibles.map(c => c.tokenId)),
+          item_quantities: JSON.stringify(collectibles.map(c => c.quantity)),
+          currencySymbol: dataCurrencyInfo?.symbol || '',
+          collectionAddress,
+          txHash
+        },
+        nums: {
+          currencyValue: Number(price),
+          currencyValueDecimal: Number(formatUnits(BigInt(price), dataCurrencyInfo?.decimals || 18))
+        }
+      })
+
+      closeSelectPaymentModal()
+
+      skipOnCloseCallback()
+
+      openTransactionStatusModal({
+        chainId,
+        currencyAddress,
+        collectionAddress,
+        txHash,
+        items: collectibles.map(collectible => ({
+          tokenId: collectible.tokenId,
+          quantity: collectible.quantity,
+          decimals: collectible.decimals,
+          price: collectible.price || price
+        })),
+        onSuccess: () => {
+          clearCachedBalances()
+          onSuccess(txHash)
+        },
+        onClose
+      })
+    } catch (e) {
+      console.error('Failed to purchase...', e)
+      onError(e as Error)
+      setIsError(true)
+    }
+
+    setIsPurchasing(false)
+  }
+
+  const onClickPurchase = () => {
+    if (compareAddress(selectedCurrency.currencyAddress, currencyAddress)) {
+      onPurchaseMainCurrency()
+    } else {
+      const foundSwap = swapRoutes
+        .flatMap(route => route.fromTokens)
+        .find(fromToken => fromToken.address.toLowerCase() === selectedCurrency.currencyAddress?.toLowerCase())
+      if (foundSwap) {
+        onClickPurchaseSwap(foundSwap)
+      }
+    }
+  }
 
   if (isLoading) {
     return (
@@ -149,16 +475,24 @@ export const PayWithCryptoTab = () => {
         </div>
       </div>
 
-      <Button
-        disabled={isPurchasing}
-        label="Confirm payment"
-        className="w-full"
-        shape="square"
-        variant="primary"
-        onClick={() => {
-          console.log('purchase logic')
-        }}
-      ></Button>
+      <div className="flex flex-col justify-start items-center w-full gap-1">
+        {isError && (
+          <div className="flex flex-col justify-start items-center w-full">
+            <Text variant="xsmall" color="negative">
+              An error occurred. Please try again.
+            </Text>
+          </div>
+        )}
+
+        <Button
+          disabled={isPurchasing}
+          label="Confirm payment"
+          className="w-full"
+          shape="square"
+          variant="primary"
+          onClick={onClickPurchase}
+        ></Button>
+      </div>
     </div>
   )
 }
