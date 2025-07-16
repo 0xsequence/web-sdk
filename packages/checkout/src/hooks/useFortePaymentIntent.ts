d7b4dfd511d24e1971b28680c20a124c1e7fdb6c
samuelea <batonenbois@gmail.com>
2025-07-16 16:39:38 -0400

    integration with sequence backend for forte

--- packages/checkout/src/hooks/useFortePaymentIntent.ts
+++ packages/checkout/src/hooks/useFortePaymentIntent.ts
@@ -2,16 +2,15 @@ import { useConfig } from '@0xsequence/hooks'
 import { useQuery } from '@tanstack/react-query'
 
 import { createFortePaymentIntent, type CreateFortePaymentIntentArgs } from '../api/data.js'
-import { useEnvironmentContext } from '../contexts/Environment.js'
 
 interface UseFortePaymentIntentOptions {
   disabled?: boolean
 }
 
 export const useFortePaymentIntent = (args: CreateFortePaymentIntentArgs, options?: UseFortePaymentIntentOptions) => {
+  // const { fortePaymentUrl } = useEnvironmentContext()
   // const { env } = useConfig()
   // const apiUrl = env.apiUrl
-  // const { fortePaymentUrl } = useEnvironmentContext()
 
   const apiUrl = 'http://localhost:4422'
 
