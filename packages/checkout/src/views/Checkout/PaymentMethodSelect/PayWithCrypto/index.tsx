7f7420541de86fbd95dd42bdccc49ee8d332c1d2
samuelea <batonenbois@gmail.com>
2025-10-27 15:03:51 -0400

    sardine removal

--- packages/checkout/src/views/Checkout/PaymentMethodSelect/PayWithCrypto/index.tsx
+++ packages/checkout/src/views/Checkout/PaymentMethodSelect/PayWithCrypto/index.tsx
@@ -462,7 +462,7 @@ export const PayWithCryptoTab = ({ skipOnCloseCallback, isSwitchingChainRef }: P
     closeSelectPaymentModal()
     triggerAddFunds({
       walletAddress: userAddress || '',
-      provider: onRampProvider || TransactionOnRampProvider.sardine,
+      provider: onRampProvider || TransactionOnRampProvider.transak,
       networks: getNetworks(),
       defaultCryptoCurrency: dataCurrencyInfo?.symbol || '',
       onClose: selectPaymentSettings?.onClose
